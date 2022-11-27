8e641f761f8713a1439db423f77897ff33e8e1d1
PIYUSH VERMA <b20163@students.iitmandi.ac.in>
2022-11-27 17:26:19 +0530

    If survey stops in between due to any reason the person can restart it

--- app.js
+++ app.js
@@ -29,20 +29,45 @@ app.use(passport.session());
 // 	useNewUrlParser: true,
 // 	useUnifiedTopology: true,
 // });
-mongoose.connect(String("mongodb+srv://Piyush:ginniferpep123@cluster0.ewfh1.mongodb.net/psycho"),{ useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(
+	String(
+		"mongodb+srv://Piyush:ginniferpep123@cluster0.ewfh1.mongodb.net/psycho"
+	),
+	{ useNewUrlParser: true, useUnifiedTopology: true }
+);
 // mongoose.set('useCreateIndex', true);
 
+// const userSchema = new mongoose.Schema(
+// 	{
+//         username : {type : String, unique:false},
+// 		name: { type: String },
+// 		email: { type: String },
+// 		age: { type: String },
+// 		gender: { type: String },
+// 		hle: { type: String },
+// 		duration: { type: String },
+// 		selections: { type: String },
+// 		primary_score: { type: String },
+// 		secondary_right: { type: String },
+// 		secondary_wrong: { type: String },
+// 	},
+// 	{ typeKey: "$type" }
+// );
 const userSchema = new mongoose.Schema({
-	name: String,
-	email: {type:String, unique:true},
-	age: String,
-	gender: String,
-	hle: String,
+	name: {type : String},
+	email: {type:String},
+	age: {type : String},
+	gender: {type : String},
+	hle: {type : String},
 	duration: {type:String, default:" . "},
 	selections: {type:String, default:" . "},
 	primary_score: {type:String, default:" . "},
 	secondary_right: {type:String, default:" . "},
 	secondary_wrong: {type:String, default:" . "},
+    attempt : {
+        type:Number,
+        default : 0
+    }
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -67,62 +92,55 @@ app.get("/", function (req, res) {
 });
 
 app.post("/newuser", function (req, res) {
-    console.log(req.body);
 	var body = req.body;
-    var found = 0;
-    User.find({email : body.email}, function(err,u){
-        if(err){
-            console.log(err);
-            res.redirect("/");
-        }
-        else{
-            found += 1;
-            console.log(u);
-            var size = Object.keys(u).length;
-            if(size==1){
-                res.send("You have previously attempted the survey!");
+	var found = 0;
+	User.find({ email: body.email }, function (err, u) {
+		if (err) {
+			res.redirect("/");
+		} else {
+            var size = u.length;
+            if(size == 0){
+                const newUser = new User({
+                    name: body.name,
+                    email: body.email,
+                    age: body.age,
+                    hle: body.hle,
+                    gender: body.gender,
+                });
+                newUser.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("New User Added");
+                    }
+                });
+                res.render("trial", { email: body.email });
             }
-        }
-    })
-    if(found===0){
-        const newUser = new User({
-            name: body.name,
-            email: body.email,
-            age: body.age,
-            hle: body.hle,
-            gender: body.gender,
-        });
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("New User Added");
+			else if (u[0].attempt==1) {
+				res.send("You have previously attempted the survey!");
+			}
+            else{
+                res.render("trial", { email: body.email });
             }
-        });
-        res.render("trial", {email : body.email});
-    }
-    
+		}
+	});
 });
 
-app.post("/dummytrial", function(req,res){
-    User.find({}, function(err,u){
-        if(err){
-            console.log(err);
-            res.redirect("/");
-        }
-        else{
-            console.log(u);
-            var size = Object.keys(u).length;
-            console.log("Size is ", size);
-            if(size%2==0){
-                res.render("questions", { email: req.body.email});
-            }
-            else{
-                res.render("questions2", { email: req.body.email});
-            }
-        }
-    })
-})
+app.post("/dummytrial", function (req, res) {
+	User.find({}, function (err, u) {
+		if (err) {
+			console.log(err);
+			res.redirect("/");
+		} else {
+			var size = u.length;
+			if (size % 2 == 0) {
+				res.render("questions", { email: req.body.email });
+			} else {
+				res.render("questions2", { email: req.body.email });
+			}
+		}
+	});
+});
 
 app.post("/dummy", function (req, res) {
 	var body = req.body;
@@ -131,26 +149,18 @@ app.post("/dummy", function (req, res) {
 		if (err) {
 			console.log(err);
 		} else {
-            event.duration = body.dur;
-            event.selections = body.sel;
-            event.primary_score = body.p_score;
-            event.secondary_right = body.sec_right;
-            event.secondary_wrong = body.sec_wrong;
-            event.save();
-        }
+			event.duration = String(body.dur);
+			event.selections = String(body.sel);
+			event.primary_score = String(body.p_score);
+			event.secondary_right = String(body.sec_right);
+			event.secondary_wrong = String(body.sec_wrong);
+            event.attempt = 1;
+			event.save();
+		}
 	});
-	console.log(req.body);
-	res.send("Thank you for attending this survey and giving us your valuable time!");
-});
-
-app.post("/test", function (req, res) {
-	console.log(req.body);
-	res.redirect("/");
-});
-
-app.post("/tip", function (req, res) {
-	console.log(req.body);
-	res.redirect("/");
+	res.send(
+		"Thank you for attending this survey and giving us your valuable time!"
+	);
 });
 
 app.listen(process.env.PORT || 5000, function () {
